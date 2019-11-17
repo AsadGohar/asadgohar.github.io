fe6f6ca4ffa6dbe5a861e1088a46fa04f888461c
Rumman Sadiq <chrumman@gmail.com>
2019-11-18 03:37:01 +0500

    initial

---
+++ precache-manifest.5c201485446a49a9da81821ae9a5ef55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11a5f3bee22dc644bb51941735f39f75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5c4896a88d0d7b3cef8",
+    "url": "/static/css/2.0f87f62a.chunk.css"
+  },
+  {
+    "revision": "b04305f55349de11640d",
+    "url": "/static/css/main.349d6985.chunk.css"
+  },
+  {
+    "revision": "e5c4896a88d0d7b3cef8",
+    "url": "/static/js/2.8a506fb1.chunk.js"
+  },
+  {
+    "revision": "b04305f55349de11640d",
+    "url": "/static/js/main.2957cab3.chunk.js"
+  },
+  {
+    "revision": "7a41912659d6c684e788",
+    "url": "/static/js/runtime-main.cbde9b69.js"
+  }
+]);
